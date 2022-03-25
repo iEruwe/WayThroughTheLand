3ffc572e04c0305ce79765f9fd920b07ce9216e1
controlledshock <100116960+controlledshock@users.noreply.github.com>
2022-03-25 16:15:39 +0700

    меню

---
+++ ger/Assets/script/UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+	public void Reload()  
+	{
+		// SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		SceneManager.LoadScene(0);
+	}
+
+	//loads inputted level
+	public void LoadLevel(string level)
+	{
+		SceneManager.LoadSceneAsync(level);
+	}
+	public void Exit()
+	{
+		Debug.Log("Exit pressed");
+		Application.Quit();
+	}
+}
